b000befc63377740f994800d8525650e123f31d0
xiaoqiang-zhao <longze_xq@163.com>
2018-08-19 15:46:10 +0800

    拆分 Demo

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-// Demo 6.1
-
-// Demo 6.
-
-// Demo 6.
-
-
+// Demo 
 
 registerServiceWorker();
